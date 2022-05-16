f6ff7f963873739fca4b19242a1981fbac15a700
Heinz N. Gies <heinz@licenser.net>
2022-05-16 13:48:52 +0200

    Fix highlighter issue with interpolation
    Signed-off-by: Heinz N. Gies <heinz@licenser.net>

--- tests/runTest.ts
+++ tests/runTest.ts
@@ -1,20 +1,20 @@
 import * as path from 'path';
 import * as fs from 'fs';
 
-import { runTests } from 'vscode-test';
+import { runTests } from '@vscode/test-electron';
 
 async function main() {
 	try {
 		// The folder containing the Extension Manifest package.json
 		// Passed to `--extensionDevelopmentPath`
 		const extensionDevelopmentPath = path.resolve(__dirname, '../../');
-		
+
 		// Minimum supported version.
 		const jsonData = fs.readFileSync(path.join(extensionDevelopmentPath, 'package.json'));
 		const json = JSON.parse(jsonData.toString());
 		let minimalVersion: string = json.engines.vscode;
 		if (minimalVersion.startsWith('^')) minimalVersion = minimalVersion.slice(1);
-	
+
 
 		// The path to the extension test script
 		// Passed to --extensionTestsPath
@@ -27,11 +27,11 @@ async function main() {
 			launchArgs: ['--disable-extensions']
 		};
 
-		const arg = { 
-			version: 'stable', 
-			extensionDevelopmentPath, 
-			extensionTestsPath, 
-			launchArgs: ['--disable-extensions'] 
+		const arg = {
+			version: 'stable',
+			extensionDevelopmentPath,
+			extensionTestsPath,
+			launchArgs: ['--disable-extensions']
 		};
 		// Download VS Code, unzip it and run the integration test
 		console.log(arg_min);
@@ -39,7 +39,7 @@ async function main() {
 		if (res_min != 0) {
 			process.exit(res_min);
 		}
-		
+
 		console.log(arg);
 		await runTests(arg)
 	} catch (err) {
--- tests/suite/trill.test.ts
+++ tests/suite/trill.test.ts
@@ -16,26 +16,11 @@ suite('Tremor Language Server', () => {
 		clearEditor(editor);
 	});
 
-	test('Test diagnostics warning trickle', async () => {
-		let [doc, _editor] = await loadTrickle(`
-		define tumbling window my_win with
-		  interval = core::datetime::with_milliseconds(100),
-		  emit_empty_windows = true
-		end;
-
-		select {"all": aggr::win::collect_flattened(event)} from in[my_win] group by event.snot into out;`);
-		let diagnostics = vscode.languages.getDiagnostics(doc.uri);
-		assertDiagnostics(diagnostics, [
-			diagnostic("Using `emit_empty_windows` without guard is potentially dangerous. Consider limiting the amount of groups maintained internally by using `max_groups` and/or `eviction_period`.", range(1, 2, 4, 5), vscode.DiagnosticSeverity.Warning, 'tremor-language-server'),
-			diagnostic("Collect functions are very expensive memory wise, try avoiding them.", range(6, 17, 6, 52), vscode.DiagnosticSeverity.Warning, 'tremor-language-server')
-		]);
-	});
-	
 	test('No diagnostics trickle', async () => {
 		let [doc, _editor] = await loadTrickle("select event from in into out;\n");
-				
+
 		let diagnostics = vscode.languages.getDiagnostics(doc.uri);
-		
+
 		assertDiagnostics(diagnostics, []);
 	})
 });
